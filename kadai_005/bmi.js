fb55211e84ea5b451defd017d955c28ba12bec46
knoageo <knoageo7@gmail.com>
2024-05-11 10:47:29 +0900

    2回目コミット

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -3,7 +3,7 @@ let userHeight
 let userBMI
 
 userWeight = 68;
-userHeight = 1.7 * 1.7;
-userBmi = userWeight/userHeight;
+userHeight = 1.7;
+userBmi = userWeight/(userHeight * userHeight);
 
 console.log(userBmi);
