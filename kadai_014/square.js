740cdeac40c0dcb3a0712d6d4ee04b2f056aa9f3
Knoageo <162757754+KNOAGEO@users.noreply.github.com>
2024-05-20 08:23:32 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,15 @@
+// 課題に対する答えはこちらになります
+const square = (num) => {
+    return num ** 2;
+}
+
+console.log(square(10));
+
+
+// 最初に思い付いたボツ定数名-------------------
+const nijo = (num) => {
+    return num ** 2;
+}
+
+console.log(nijo(20));
+//--------------------------------------------
